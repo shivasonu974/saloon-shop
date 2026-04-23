20c156681dd4c4d0f822092847e052da730841f7
Shiva Sonu <shivasonu974@gmail.com>
2026-04-24 02:34:26 +0530

    Fix Vite resolving absolute path on Render

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/', // 👈 ADD THIS LINE (VERY IMPORTANT)
-
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
